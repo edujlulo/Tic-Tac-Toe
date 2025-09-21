998caa544ca454da1ec2df205810093323d057ad
Eduardo Lulo <eduardo.lulo.m@gmail.com>
2025-09-21 10:58:57 +0100

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,53 +4,17 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = useState("");
-
-  let player = "X";
-
-  function changePlayer() {
-    if (player === "X") {
-      player = "O";
-    } else {
-      player = "X";
-    }
-    console.log("hey");
-  }
-
-  function displayMove() {
-    setValue(player);
-  }
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [player, setPlayer] = useState("X");
 
   return (
     <>
       <div id="board">
-        <div className="box" onClick={displayMove}>
-          {value}
-        </div>
-        <div className="box" onClick={displayMove}>
-          {value}
-        </div>
-        <div className="box" onClick={displayMove}>
-          {value}
-        </div>
-        <div className="box" onClick={displayMove}>
-          {value}
-        </div>
-        <div className="box" onClick={displayMove}>
-          {value}
-        </div>
-        <div className="box" onClick={displayMove}>
-          {value}
-        </div>
-        <div className="box" onClick={displayMove}>
-          {value}
-        </div>
-        <div className="box" onClick={displayMove}>
-          {value}
-        </div>
-        <div className="box" onClick={displayMove}>
-          {value}
-        </div>
+        {board.map((value, index) => (
+          <div key={index} className="box">
+            {value}
+          </div>
+        ))}
       </div>
       <p id="turnMessage">It's {player}'s turn</p>
     </>
